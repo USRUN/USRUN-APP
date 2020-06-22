b1f1bb41c92e07ea873b3c9f0887936a66007dc7
Kha Tran <1653032@student.hcmus.edu.vn>
2020-06-22 10:19:29 +0700

    wip: fetching team info in team_info

---
+++ usrun/lib/model/team_summary.dart
@@ -0,0 +1,14 @@
+import 'package:usrun/core/define.dart';
+
+import 'package:usrun/util/reflector.dart';
+
+import 'package:usrun/model/mapper_object.dart';
+
+
+@reflector
+class TeamSummary extends MapperObject {
+  int teamId;
+  String teamName;
+  int teamMemberCount;
+  String thumbnail;
+}
