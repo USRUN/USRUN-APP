ba5acb8040380197baae59759d5c2a5bdf14f421
Ngoc Vo <ngocvtt@vng.comvn>
2020-06-23 13:18:19 +0700

    finish all sign in adapter

--- usrun/android/app/src/main/kotlin/com/example/usrun/MainActivity.kt
+++ usrun/android/app/src/main/kotlin/com/example/usrun/MainActivity.kt
@@ -1,13 +1,33 @@
 package com.example.usrun
 
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Base64
+import android.util.Base64.DEFAULT
+import android.util.Base64.encodeToString
+import android.util.Log
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 class MainActivity: FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
+    try {
+      val info: PackageInfo = packageManager.getPackageInfo(
+              "vn.hcmus.usrun",
+              PackageManager.GET_SIGNATURES)
+      for (signature in info.signatures) {
+        val md: MessageDigest = MessageDigest.getInstance("SHA")
+        md.update(signature.toByteArray())
+        Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+      }
+    } catch (e: PackageManager.NameNotFoundException) {
+    } catch (e: NoSuchAlgorithmException) {
+    }
   }
 }
