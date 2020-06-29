9100ebfd865c230752f75e542160f4f02dc55a15
quoctk <1653072@student.hcmus.edu.vn>
2020-06-29 20:57:21 +0700

    Minor update

--- usrun/lib/model/event.dart
+++ usrun/lib/model/event.dart
@@ -1,25 +1,20 @@
-import 'package:usrun/core/define.dart';
-
 import 'package:usrun/util/reflector.dart';
-
 import 'package:usrun/model/mapper_object.dart';
-
 
 @reflector
 class Event extends MapperObject {
   int eventId;
-  String name;
-  SportType sportType;
-  String description;
-  DateTime startDate;
-  DateTime endDate;
-  DateTime updateDate;
-  EventType type;
-  String img;
-  String logo;
-  bool isActive;
-  int memberCount;
-  int teamCount;
-  num distance;
-  UserRole userType;
+  int status;
+  DateTime createTime;
+  String eventName;
+  String subtitle;
+  String thumbnail;
+  String poster;
+  String banner;
+  int totalDistance;
+  int totalTeamParticipant;
+  int totalParticipant;
+  DateTime startTime;
+  DateTime endTime;
+  String sponsorName;
 }
