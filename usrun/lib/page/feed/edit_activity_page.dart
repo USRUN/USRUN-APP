30425c59ad057f7d75742bd5d2a54fde3e469af3
quoctk <1653072@student.hcmus.edu.vn>
2020-08-02 22:38:40 +0700

    Update edit_activity_page.dart

---
+++ usrun/lib/page/feed/edit_activity_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class EditActivityPage extends StatefulWidget {
+  @override
+  _EditActivityPageState createState() => _EditActivityPageState();
+}
+
+class _EditActivityPageState extends State<EditActivityPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
